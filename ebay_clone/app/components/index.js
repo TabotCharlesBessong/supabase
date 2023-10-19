5c0664d92b6d150d0349d8999e435d939b8e1f4b
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-19 17:52:05 +0100

    product and loading component

--- ebay_clone/app/components/index.js
+++ ebay_clone/app/components/index.js
@@ -1,2 +1,4 @@
 
 export {default as CarouselComp} from "./CarouselComp"
+export {default as Product} from "./Product"
+export {default as Loading} from "./Loading"
--- ebay_clone/app/page.js
+++ ebay_clone/app/page.js
@@ -1,13 +1,47 @@
 
 "use client"
 
-import { CarouselComp } from "./components"
+import { useState } from "react";
+import { CarouselComp, Product } from "./components"
 import MainLayout from "./layouts/MainLayout"
 
 export default function Home() {
+  // const [products, setProducts] = useState([]);
+
+  const products = [
+    {
+      id: 1,
+      url: "images/banner/1.png",
+      title: "Product 1",
+      price: 999,
+    },
+    {
+      id: 2,
+      url: "images/banner/2.png",
+      title: "Product 2",
+      price: 1499,
+    },
+    {
+      id: 3,
+      url: "images/banner/3.png",
+      title: "Product 3",
+      price: 799,
+    },
+    // Add more dummy products as needed
+  ];
+
   return (
     <MainLayout>
       <CarouselComp />
+      <div className="max-w-[1200px] mx-auto">
+        <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+
+        <div className="grid grid-cols-5 gap-4">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
     </MainLayout>
-  )
+  );
 }
