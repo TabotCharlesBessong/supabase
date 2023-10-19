5d7d7b9ab33600842e71fe55dc749234eff22caa
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-19 17:31:37 +0100

    carousel component

--- ebay_clone/app/page.js
+++ ebay_clone/app/page.js
@@ -1,12 +1,13 @@
 
 "use client"
 
+import { CarouselComp } from "./components"
 import MainLayout from "./layouts/MainLayout"
 
 export default function Home() {
   return (
     <MainLayout>
-      Cool
+      <CarouselComp />
     </MainLayout>
   )
 }
