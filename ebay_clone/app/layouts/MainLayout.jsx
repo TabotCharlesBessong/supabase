a42d9bfaf0c5dd6a783f7b915cae97bafe0eb1f2
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-20 12:26:16 +0100

    loading function

--- ebay_clone/app/layouts/MainLayout.jsx
+++ ebay_clone/app/layouts/MainLayout.jsx
@@ -1,29 +1,38 @@
 "use client";
 
-import React from "react";
 import TopMenu from "./includes/TopMenu";
 import MainHeader from "./includes/MainHeader";
 import SubMenu from "./includes/SubMenu";
 import Footer from "./includes/Footer";
+import Loading from "../components/Loading";
+import { useEffect, useState } from "react";
 
-const MainLayout = ({ children }) => {
+export default function MainLayout({ children }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("storage", function () {
+      let res = localStorage.getItem("isLoading");
+      res === "false" ? setIsLoading(false) : setIsLoading(true);
+    });
+  });
+
   return (
     <>
       <div id="MainLayout" className="min-w-[1050px] max-w-[1300px] mx-auto">
         <div>
+          {isLoading ? <Loading /> : <div></div>}
           <TopMenu />
           <MainHeader />
           <SubMenu />
         </div>
-        <div>
-          {children}
-        </div>
+
+        <div>{children}</div>
+
         <div>
           <Footer />
         </div>
       </div>
     </>
   );
-};
-
-export default MainLayout;
+}
